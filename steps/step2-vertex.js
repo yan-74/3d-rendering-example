fd6fbbf803eac770990b34af8f0a21f286518ff1
ngyan74 <ngyanchi11@gmail.com>
2026-03-31 22:35:02 -0400

    Add more transformations to vertex shading

--- steps/step2-vertex.js
+++ steps/step2-vertex.js
@@ -1,7 +1,16 @@
 //STEP 2: Vertex shading
 
-//To simulate the transformations done in vertex shading, we can rotate the cube in the animation loop!
-//This animation is run every frame
+//Simulate the transformations done in vertex shading, these animations are run every frame
+//Pick any animation and try them out!
 
+//rotate
 cube.rotation.x += 0.01;
 cube.rotation.y += 0.01;
+
+//scale
+cube.scale.x = 1 + 0.5 * Math.sin(Date.now() * 0.002);
+cube.scale.y = 1 + 0.5 * Math.cos(Date.now() * 0.002);
+
+//move the camera!
+camera.position.x = 5 * Math.sin(Date.now() * 0.001);
+camera.lookAt(cube.position);
